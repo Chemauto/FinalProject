494fbc5bb7092f6c521ce032668db4512884d670
ustbxcj <2248190184@qq.com>
2026-03-26 15:03:27 +0800

    更新视觉vlm的模型使用

--- VLM_Module/vlm_core.py
+++ VLM_Module/vlm_core.py
@@ -40,7 +40,7 @@ class VLMCore:
         self,
         api_key: str | None = None,
         base_url: str = "https://dashscope.aliyuncs.com/compatible-mode/v1",
-        model: str = "qwen3-vl-plus",
+        model: str = "qwen3-vl-235b-a22b-thinking",
         temperature: float = 0.1,
         Video_Port: int | str = DEFAULT_VIDEO_PORT,
         default_image: str | None = DEFAULT_IMAGE_PATH,
